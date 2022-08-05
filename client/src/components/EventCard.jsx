60d9692ba56178d9196a3757bf8d2db4b4a8bd29
Andrés Vilá <vilaandres@gmail.com>
2022-08-05 15:19:18 -0500

    Finished events section

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./EventCard.module.css";
+import favYes from "../icons/favorite_FILL1_wght400_GRAD0_opsz48.svg";
+import favNo from "../icons/favorite_FILL0_wght400_GRAD0_opsz48.svg";
 
 export default function EventCard(props) {
+  const [displayDesc, setDisplayDesc] = useState(false);
   const days = [
     "Sunday",
     "Monday",
@@ -26,22 +29,45 @@ export default function EventCard(props) {
     "December",
   ];
   const fullDate = new Date(props.date);
-  console.log(
-    "🚀 ~ file: EventCard.jsx ~ line 29 ~ EventCard ~ fullDate",
-    fullDate
-  );
-  const day = days[fullDate.getDay];
-  const month = months[fullDate.getMonth];
-  // {`${day} ${fullDate.getDay} ${month}`}
-  // const year = fullDate.getFullYear();
+  const day = days[fullDate.getDay()];
+  const month = months[fullDate.getMonth()].toLowerCase();
+
+  function handleClick() {
+    setDisplayDesc((old) => !old);
+  }
+  const description = displayDesc ? props.desc : "";
+
   return (
-    <div className={style.container}>
-      <img
-        alt="The band"
-        src={"http://localhost:3001/" + props.image}
-        width="150px"
-      />
-      <div className={style.eventData}>{props.date}</div>
-    </div>
+    <>
+      <div className={style.container} onClick={handleClick}>
+        <img
+          alt="The band"
+          src={"http://localhost:3001/" + props.image}
+          width="150px"
+        />
+        <div className={style.eventData}>
+          <h3 className={style.eventTitle}>{props.title}</h3>
+          <p>
+            <span className="material-symbols-outlined">calendar_month</span>
+            {`${day} ${fullDate.getDay()} ${month}`}
+          </p>
+          <p>
+            <span className="material-symbols-outlined">schedule</span>
+            {`${fullDate.getHours()}:${fullDate.getMinutes()}`}
+          </p>
+          <p>
+            <span className="material-symbols-outlined">location_on</span>
+            {props.location}
+          </p>
+        </div>
+        <img
+          className={style.isFavorite}
+          alt={props.isFavorite ? "Favorite" : "Not favorite"}
+          src={props.isFavorite ? favYes : favNo}
+          onClick={() => props.togFav(props.id)}
+        />
+      </div>
+      {description}
+    </>
   );
 }
--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -5,32 +5,102 @@ import EventCard from "./EventCard";
 import style from "./Events.module.css";
 
 export default function Events() {
-  const [events, setEvents] = useState([]);
+  const [fetchedEvents, setFetchedEvents] = useState([]);
+  const [fetchedFavs, setFetchedFavs] = useState([]);
 
-  const eventCards = events.map((event) => (
-    <EventCard
-      title={event.title}
-      date={event.date}
-      image={event.image}
-      location={event.location}
-    />
-  ));
+  const [showEvents, setShowEvents] = useState([]);
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("all");
+  const [date_time, setDate_time] = useState("all");
+
+  const today = Date.now();
+
+  async function toggleFav(eventId) {
+    await axios.post(
+      "http://localhost:3001/user/ddf40198-fc6c-4595-95cc-bda6d77fffaa/favEvent/" +
+        eventId
+    );
+    fetchFavorites();
+  }
+
+  const eventCards =
+    showEvents.length > 0 ? (
+      showEvents.map((event) => (
+        <EventCard
+          key={event.id}
+          id={event.id}
+          title={event.title}
+          date={event.date}
+          image={event.image}
+          location={event.location}
+          desc={event.description}
+          isFavorite={
+            !!fetchedFavs.find((favEvent) => event.id === favEvent.id)
+          }
+          togFav={toggleFav}
+        />
+      ))
+    ) : (
+      <p>No events to show</p>
+    );
 
   useEffect(() => {
     fetchEvents();
+    fetchFavorites();
   }, []);
   const fetchEvents = async () => {
-    const response = await axios.get("http://localhost:3001/event");
-    setEvents(response.data);
+    const events = await axios.get("http://localhost:3001/event");
+    setFetchedEvents(events.data);
+    setShowEvents(events.data);
   };
+  const fetchFavorites = async () => {
+    const favs = await axios.get(
+      "http://localhost:3001/favorite/ddf40198-fc6c-4595-95cc-bda6d77fffaa/event"
+    );
+    setFetchedFavs(favs.data);
+  };
+
+  // ************ FILTERS
+  function handleCategory(e) {
+    setCategory(e.target.value);
+  }
+  function handleSearch(e) {
+    setSearch(e.target.value);
+  }
+  function handleDate_time(e) {
+    setDate_time(e.target.value);
+  }
+  useEffect(() => {
+    setShowEvents(
+      fetchedEvents.filter(
+        (event) =>
+          (search !== ""
+            ? event.title.toLowerCase().includes(search.toLowerCase())
+            : true) &&
+          (category !== "all" ? event.category === category : true) &&
+          (date_time !== "all" && date_time === "upcoming"
+            ? Date.parse(event.date) > today
+            : Date.parse(event.date) < today)
+      )
+    );
+  }, [search, category, date_time]);
 
   return (
     <div className={style.container}>
       <h1 className={style.title}>Events</h1>
       <nav className={style.events_nav}>
-        <input type="text" size="15" />
-        <input type="text" size="15" />
-        <input type="text" size="15" />
+        <select name="category" onChange={handleCategory}>
+          <option value="all">All categories</option>
+          <option value="drifting">Drifting</option>
+          <option value="guest">Guest</option>
+          <option value="music">Music</option>
+        </select>
+        <select name="date_time" onChange={handleDate_time}>
+          <option value="all">All dates</option>
+          <option value="upcoming">Upcoming</option>
+          <option value="past">Past</option>
+        </select>
+        <input type="text" size="15" onChange={handleSearch} />
       </nav>
       <main>{eventCards}</main>
     </div>
