da0bdcca236013527f4849acffeddda2a39276b1
Andrés Vilá <vilaandres@gmail.com>
2022-08-15 15:23:20 -0500

    Deploy added axis.defaults

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./index.css";
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 import dotenv from "dotenv";
 dotenv.config();
 
-// axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
