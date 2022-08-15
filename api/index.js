c66fa3e5418ecd5e96b0d9b27de91c82d544f919
Andrés Vilá <vilaandres@gmail.com>
2022-08-15 17:49:54 -0500

    Testing bulkCreate4

--- api/index.js
+++ api/index.js
@@ -34,30 +34,30 @@ conn.sync({ force: false, alter: false }).then(() => {
   //     ignoreDuplicates: true,
   //   }
   // );
-  UserCategory.bulkCreate(
-    [
-      {
-        id: "5f40db04-7e21-4e88-acaa-87d5928f7ebb",
-        title: "User",
-        category: "U",
-      },
-    ],
-    {
-      ignoreDuplicates: true,
-    }
-  );
-  Wallet.bulkCreate(
-    [
-      {
-        id: "147a9663-e722-4667-b54e-44b5817e0bd8",
-        liquid: 10000,
-        frozen: 0,
-      },
-    ],
-    {
-      ignoreDuplicates: true,
-    }
-  );
+  // UserCategory.bulkCreate(
+  //   [
+  //     {
+  //       id: "5f40db04-7e21-4e88-acaa-87d5928f7ebb",
+  //       title: "User",
+  //       category: "U",
+  //     },
+  //   ],
+  //   {
+  //     ignoreDuplicates: true,
+  //   }
+  // );
+  // Wallet.bulkCreate(
+  //   [
+  //     {
+  //       id: "147a9663-e722-4667-b54e-44b5817e0bd8",
+  //       liquid: 10000,
+  //       frozen: 0,
+  //     },
+  //   ],
+  //   {
+  //     ignoreDuplicates: true,
+  //   }
+  // );
   // User.bulkCreate(
   //   [
   //     {
@@ -72,121 +72,121 @@ conn.sync({ force: false, alter: false }).then(() => {
   //     ignoreDuplicates: true,
   //   }
   // );
-  Car.bulkCreate(
-    [
-      {
-        id: "99ac1b58-3926-45f8-8b73-2a199025319d",
-        title: "Z-Tuned Skyline GT-R",
-        number: 23,
-        manufacturer: "GT",
-        tireManufacturer: "Yokohama",
-        chasis: "AZ-76",
-        description:
-          "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
-        geolocation: "174.2,65.93",
-        festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
-        carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
-      },
-      {
-        id: "99ac1b58-3926-45f8-8b73-2a199025318d",
-        title: "Rx7 Mazda",
-        number: 73,
-        manufacturer: "Mazda",
-        tireManufacturer: "Yokohama",
-        chasis: "EWZ-46",
-        description:
-          "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
-        geolocation: "174.2,65.93",
-        festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
-        carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
-      },
-      {
-        id: "99ac1b58-3926-45f8-8b73-2a199025318a",
-        title: "1997 eclipse f&f replica",
-        number: 1984,
-        manufacturer: "GM",
-        tireManufacturer: "Yokohama",
-        chasis: "EWQ-9",
-        description:
-          "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
-        geolocation: "174.2,65.93",
-        festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
-        carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
-      },
-      {
-        id: "99ac1b58-3926-45f8-8b73-2a199025319c",
-        title: "BDM ARCH 65",
-        number: 124,
-        manufacturer: "BDM",
-        tireManufacturer: "Michelin",
-        chasis: "Q29",
-        description:
-          "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
-        geolocation: "174.2,65.93",
-        festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
-        carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
-      },
-      {
-        id: "7ad0230c-81a1-402f-8baa-922f6d8bba7d",
-        title: "TYRALM 34X",
-        number: 34,
-        manufacturer: "TYRALM",
-        tireManufacturer: "Goodyear",
-        chasis: "27Ty",
-        description:
-          "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
-        geolocation: "174.2,65.93",
-        festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
-        carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
-      },
-    ],
-    {
-      ignoreDuplicates: true,
-    }
-  );
-  // CarImage.bulkCreate(
+  // Car.bulkCreate(
   //   [
   //     {
-  //       image: "uploads/car/car-1660054875610.jpg",
-  //       carId: "99ac1b58-3926-45f8-8b73-2a199025319c",
+  //       id: "99ac1b58-3926-45f8-8b73-2a199025319d",
+  //       title: "Z-Tuned Skyline GT-R",
+  //       number: 23,
+  //       manufacturer: "GT",
+  //       tireManufacturer: "Yokohama",
+  //       chasis: "AZ-76",
+  //       description:
+  //         "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
+  //       geolocation: "174.2,65.93",
+  //       festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
+  //       carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
   //     },
   //     {
-  //       image: "uploads/car/car-1660054875616.jpg",
-  //       carId: "99ac1b58-3926-45f8-8b73-2a199025319c",
+  //       id: "99ac1b58-3926-45f8-8b73-2a199025318d",
+  //       title: "Rx7 Mazda",
+  //       number: 73,
+  //       manufacturer: "Mazda",
+  //       tireManufacturer: "Yokohama",
+  //       chasis: "EWZ-46",
+  //       description:
+  //         "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
+  //       geolocation: "174.2,65.93",
+  //       festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
+  //       carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
   //     },
   //     {
-  //       image: "uploads/car/GT03.png",
-  //       carId: "99ac1b58-3926-45f8-8b73-2a199025319d",
+  //       id: "99ac1b58-3926-45f8-8b73-2a199025318a",
+  //       title: "1997 eclipse f&f replica",
+  //       number: 1984,
+  //       manufacturer: "GM",
+  //       tireManufacturer: "Yokohama",
+  //       chasis: "EWQ-9",
+  //       description:
+  //         "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
+  //       geolocation: "174.2,65.93",
+  //       festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
+  //       carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
   //     },
   //     {
-  //       image: "uploads/car/RX71.png",
-  //       carId: "99ac1b58-3926-45f8-8b73-2a199025318d",
+  //       id: "99ac1b58-3926-45f8-8b73-2a199025319c",
+  //       title: "BDM ARCH 65",
+  //       number: 124,
+  //       manufacturer: "BDM",
+  //       tireManufacturer: "Michelin",
+  //       chasis: "Q29",
+  //       description:
+  //         "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
+  //       geolocation: "174.2,65.93",
+  //       festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
+  //       carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
   //     },
   //     {
-  //       image: "uploads/car/RX72.png",
-  //       carId: "99ac1b58-3926-45f8-8b73-2a199025318d",
-  //     },
-  //     {
-  //       image: "uploads/car/GM1.png",
-  //       carId: "99ac1b58-3926-45f8-8b73-2a199025318a",
-  //     },
-  //     {
-  //       image: "uploads/car/GM2.png",
-  //       carId: "99ac1b58-3926-45f8-8b73-2a199025318a",
-  //     },
-  //     {
-  //       image: "uploads/car/car-1659971663360.jpg",
-  //       carId: "7ad0230c-81a1-402f-8baa-922f6d8bba7d",
-  //     },
-  //     {
-  //       image: "uploads/car/car-1659971663362.jpg",
-  //       carId: "7ad0230c-81a1-402f-8baa-922f6d8bba7d",
+  //       id: "7ad0230c-81a1-402f-8baa-922f6d8bba7d",
+  //       title: "TYRALM 34X",
+  //       number: 34,
+  //       manufacturer: "TYRALM",
+  //       tireManufacturer: "Goodyear",
+  //       chasis: "27Ty",
+  //       description:
+  //         "Placerat in egestas erat imperdiet sed euismod. Ut tellus elementum sagittis vitae et. Morbi tristique senectus et netus et malesuada fames ac.",
+  //       geolocation: "174.2,65.93",
+  //       festivalId: "40f41d79-21ae-4db8-8d1d-bb831eabc337",
+  //       carOwnerId: "12dcd2f1-3e86-4fb9-bc07-4f90c314808b",
   //     },
   //   ],
   //   {
   //     ignoreDuplicates: true,
   //   }
   // );
+  CarImage.bulkCreate(
+    [
+      {
+        image: "uploads/car/car-1660054875610.jpg",
+        carId: "99ac1b58-3926-45f8-8b73-2a199025319c",
+      },
+      {
+        image: "uploads/car/car-1660054875616.jpg",
+        carId: "99ac1b58-3926-45f8-8b73-2a199025319c",
+      },
+      {
+        image: "uploads/car/GT03.png",
+        carId: "99ac1b58-3926-45f8-8b73-2a199025319d",
+      },
+      {
+        image: "uploads/car/RX71.png",
+        carId: "99ac1b58-3926-45f8-8b73-2a199025318d",
+      },
+      {
+        image: "uploads/car/RX72.png",
+        carId: "99ac1b58-3926-45f8-8b73-2a199025318d",
+      },
+      {
+        image: "uploads/car/GM1.png",
+        carId: "99ac1b58-3926-45f8-8b73-2a199025318a",
+      },
+      {
+        image: "uploads/car/GM2.png",
+        carId: "99ac1b58-3926-45f8-8b73-2a199025318a",
+      },
+      {
+        image: "uploads/car/car-1659971663360.jpg",
+        carId: "7ad0230c-81a1-402f-8baa-922f6d8bba7d",
+      },
+      {
+        image: "uploads/car/car-1659971663362.jpg",
+        carId: "7ad0230c-81a1-402f-8baa-922f6d8bba7d",
+      },
+    ],
+    {
+      ignoreDuplicates: true,
+    }
+  );
   // CarOwner.bulkCreate(
   //   [
   //     {
