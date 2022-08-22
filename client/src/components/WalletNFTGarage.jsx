6cc79f3629d88867f2178689fbc57eb518b42eba
Andrés Vilá <vilaandres@gmail.com>
2022-08-22 09:32:38 -0500

    Placeholder images for NFT Garage

--- client/src/components/WalletNFTGarage.jsx
+++ client/src/components/WalletNFTGarage.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import MainContainer from "../assets/MainContainer";
 import Title from "../assets/Title";
+import placeholder1 from "../img/NFTGarage_placeholder1.jpeg";
+import placeholder2 from "../img/NFTGarage_placeholder2.jpeg";
 
 export default function WalletNFTGarage() {
+  const images = [placeholder1, placeholder2];
+  const [counter, setCounter] = useState(0);
+  const [image, setImage] = useState(images[0]);
+  function handleImage() {
+    setCounter((prev) => (prev === 2 ? 0 : prev + 1));
+    setImage(images[counter]);
+  }
   return (
     <MainContainer>
-      {<Title title="NFT Garage" backButton="true" />}
+      <Title title="NFT Garage" backButton="true" />
+      <img alt="phd" src={image} onClick={handleImage} />
     </MainContainer>
   );
 }
