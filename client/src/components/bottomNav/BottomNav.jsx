29f6cad7bf89ca49157a2fa206964a814efa2a88
Andrés Vilá <vilaandres@gmail.com>
2022-10-21 18:16:14 -0500

    BottomNav is position:fixed again cause it wasnt showing on mobile

--- client/src/components/bottomNav/BottomNav.jsx
+++ client/src/components/bottomNav/BottomNav.jsx
@@ -7,8 +7,8 @@ import styled from "styled-components";
 
 const StyledContainer = styled.nav`
   background-color: rgb(0, 0, 0);
-  /* position: fixed;
-  bottom: 0; */
+  position: fixed;
+  bottom: 0;
   width: 100%;
   z-index: 10000;
 `;
